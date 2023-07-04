e5e7a99b026ca77fb865d5c35ded62f7ca92437e
Alan Suchowolski <asucho3@gmail.com>
2023-07-04 14:33:01 -0300

    minor fixes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 // Because the back end and the front end are in different ports/locations, we must specify the origin and the credentials
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: "https://imgsuch.netlify.app/",
     credentials: true,
   })
 );
